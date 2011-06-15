5582b4ca7f3cb7747032029014fd05e64902b1ed
Jakob Torp <jakob@revealit.dk>
2011-06-15 21:36:21 +0200

    Improved flat rate plugin.

--- plugins/quotes/flat_rate/CommerceShippingFlatRate.class.php
+++ plugins/quotes/flat_rate/CommerceShippingFlatRate.class.php
@@ -2,6 +2,8 @@
 
 class CommerceShippingFlatRate extends CommerceShippingQuote {
   public function settings_form(&$form, $rules_settings) {
+    $currencies = commerce_currencies(TRUE);
+
     $form['shipping_price'] = array(
       '#type' => 'textfield',
       '#title' => t('Shipping rate'),
@@ -9,6 +11,26 @@ class CommerceShippingFlatRate extends CommerceShippingQuote {
       '#default_value' => is_array($rules_settings) && isset($rules_settings['shipping_price']) ? $rules_settings['shipping_price'] : 42,
       '#element_validate' => array('rules_ui_element_decimal_validate'),
     );
+
+    if (count($currencies) > 1) {
+      $form['shipping_price']['#title'] = t('Default rate');
+      $form['shipping_price']['#description'] = t("Note multi currency doesn't work well with commerce yet, due some bad price handlling in commerce.");
+      $form['shipping_rates'] = array(
+        '#type' => 'fieldset',
+        '#title' => t('Shipping rates'),
+        '#collapsed' => TRUE,
+      );
+
+      foreach ($currencies as $currency_code => $currency) {
+        $form['shipping_rates'][$currency_code] = array(
+          '#type' => 'textfield',
+          '#title' => t('Shipping rate (@code)', array('@code' => $currency_code)),
+          '#description' => t('Configure what the rate should be.'),
+          '#default_value' => is_array($rules_settings) && isset($rules_settings['shipping_rates'][$currency_code]) ? $rules_settings['shipping_rates'][$currency_code] : 42,
+          '#element_validate' => array('rules_ui_element_decimal_validate'),
+        );
+      }
+    }
 
     $form['rate_type'] = array(
       '#type' => 'select',
@@ -18,6 +40,7 @@ class CommerceShippingFlatRate extends CommerceShippingQuote {
       '#options' => array(
         'product' => t('Product'),
         'line_item' => t('Product types (line items)'),
+        'order' => t('Order'),
       ),
     );
 
@@ -34,6 +57,12 @@ class CommerceShippingFlatRate extends CommerceShippingQuote {
     }
     $settings = $this->settings;
     $order_wrapper = entity_metadata_wrapper('commerce_order', $order);
+    if (isset($settings['shipping_rates'][$currency_code]) && $settings['shipping_rates'][$currency_code]) {
+      $amount = $settings['shipping_rates'][$currency_code];
+    }
+    else {
+      $amount = $settings['shipping_price'];
+    }
 
     $quantity = 0;
     foreach ($order_wrapper->commerce_line_items as $line_item_wrapper) {
@@ -44,11 +73,14 @@ class CommerceShippingFlatRate extends CommerceShippingQuote {
         elseif ($settings['rate_type'] == 'line_item') {
           $quantity += 1;
         }
+        elseif ($settings['rate_type'] == 'order') {
+          $quantity = 1;
+        }
       }
     }
     $shipping_line_items = array();
     $shipping_line_items[] = array(
-      'amount' => commerce_currency_decimal_to_amount($settings['shipping_price'], $currency_code),
+      'amount' => commerce_currency_decimal_to_amount($amount, $currency_code),
       'currency_code' => $currency_code,
       'label' => $settings['label'],
       'quantity' => $quantity,
